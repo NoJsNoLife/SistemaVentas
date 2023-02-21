b7c2ce65ff1901c3434ed6c5fe9261eeb703809d
NoJsNoLife <fnayala.jr@gmail.com>
2023-02-20 21:06:32 -0300

    First Commit

---
+++ src/main/java/main/SistemaVentas.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package main;
+
+/**
+ *
+ * @author Fer
+ */
+public class SistemaVentas {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
